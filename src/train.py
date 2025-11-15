7e1993d90e686fd007af4edce643493fc7a16152
Dinh Tuan <dndtuan.ly.cr@gmail.com>
2025-11-15 17:00:27 +0700

    update new model simple

--- src/train.py
+++ src/train.py
@@ -14,7 +14,7 @@ import torch.nn.functional as F
 
 # --- import your modules ---
 from model import VideoEncoder, contrastive_loss
-from dataset import FeatureVideoQAHME_MC_CLIP, CLIPTextEncoder, collate_fn_hme_clip
+from dataset import FeatureVideoQAHME_MC_LLMAssist, CLIPTextEncoder, collate_fn_hme_clip
 
 # -------------------------
 # Config / Hyperparameters
@@ -104,7 +104,7 @@ def train_loop(args):
     # Dataset & DataLoader
     # --------------------------
     print("Preparing dataset...")
-    full_ds = FeatureVideoQAHME_MC_CLIP(
+    full_ds = FeatureVideoQAHME_MC_LLMAssist(
         json_path=args["data_json"],
         feature_dir_appearance=args["feat_app_dir"],
         feature_dir_motion=args["feat_mot_dir"],
